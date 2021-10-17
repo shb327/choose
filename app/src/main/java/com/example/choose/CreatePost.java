1ad9dd866f282c46d01d3e4243ce1395838fea2a
shb327 <shkamarida.b@gmail.com>
2021-10-17 22:04:51 +0300

    Initial Commit

--- app/src/main/java/com/example/choose/CreatePost.java
+++ app/src/main/java/com/example/choose/CreatePost.java
@@ -1,13 +1,26 @@
 package com.example.choose;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreatePost extends AppCompatActivity {
+import java.sql.SQLOutput;
+
+public class CreatePost extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+    }
+
+    public void onButtonClick(View view){
+        EditText editText = findViewById(R.id.editText);
+        TextView txt = findViewById(R.id.textView);
+        txt.setText(editText.getText().toString());
     }
 }
