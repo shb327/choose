b3dbc4e43dcb9f63c4da0414d83b4840ae383f5d
shb327 <shkamarida.b@gmail.com>
2021-10-18 17:11:40 +0300

    Two Activities

--- app/src/main/java/com/example/choose/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/choose/ui/login/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.choose.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.choose.CreatePost;
 import com.example.choose.R;
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn = (Button)findViewById(R.id.login_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CreatePost.class));
+            }
+        });
     }
 }
